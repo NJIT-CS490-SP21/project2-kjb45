d556cf6cacb35f1b9b24486e67442203dbd343d4
EC2 Default User <ec2-user@ip-172-31-40-227.ec2.internal>
2021-02-25 08:40:11 +0000

    got X to appear on board/board isnt updating on the first pass

--- src/Board.js
+++ src/Board.js
@@ -6,20 +6,27 @@ import { Box } from './Box.js'
 
 export function Board(props){
 
-const [board, setBoard] = useState([8]);
+const [board, setBoard] = useState([]);
 
 function clicked(index){
     //const userInput = useRef.current.value;
     //let newBoard = [];
     console.log("box clicked");
     setBoard(prevBoard => [...prevBoard,'X']);
+    console.log(board);
+    var move = board[0];
+    console.log(move);
     //let item = newBoard[index];
     //console.log(index);
 }
 
+let move = board[0];
+
+
 return (
     <div className="board">
-        <Box onClick={() => clicked(0)}/>
+        
+        <Box onClick={() => clicked(0)} move={move}/>
         <Box />
         <Box />
         <Box />
