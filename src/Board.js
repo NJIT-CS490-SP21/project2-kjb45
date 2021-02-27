470d24a9f756ab13120c2c8f4225f89942f2c3b7
EC2 Default User <ec2-user@ip-172-31-40-227.ec2.internal>
2021-02-27 22:51:58 +0000

    update Board.js and it is now emitting moves and moves are staying on the screen

--- src/Board.js
+++ src/Board.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Board.css';
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { Box } from './Box.js'
+import io from 'socket.io-client';
 
 
+const socket = io(); //connects to socket connection
 export function Board(props){
 
 const [board, setBoard] = useState([' ',' ',' ',' ',' ',' ',' ',' ',' ']);
@@ -17,12 +19,32 @@ function clicked(index){
         const boardCopy = [...prevBoard];
         
         (move === 0 ? (boardCopy[index] = 'X') : boardCopy[index] = 'O');
+        socket.emit('board', {board: boardCopy});
         return boardCopy;
+        
     });
     
     (move === 0 ? (setMove(1)) : setMove(0));
 
+
 }
+
+
+useEffect(() => {
+    //listening for a new move event
+    //run the code in the function that is passed in as the second arg
+    
+    socket.on('board', (data) => {
+       console.log('New move event recieved');
+       console.log(data);
+       setBoard(prevBoard => {
+           let boardCopy = [...prevBoard]; 
+           boardCopy = data['board'];
+           return boardCopy;
+       });
+       
+    });
+}, []);
 
 console.log(board);
 console.log(move);
