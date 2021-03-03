a9e10facf5750ef2a71759b42c3d04585ccae8cc
EC2 Default User <ec2-user@ip-172-31-40-227.ec2.internal>
2021-03-03 10:46:57 +0000

    fixed faulty win states in board.js

--- src/Board.js
+++ src/Board.js
@@ -21,14 +21,62 @@ const [playx, setPlayX] = useState('');
 const [playo, setPlayO] = useState('');
 const [gameComplete, setGameComplete] = useState(false);
 const [plays, setPlays] = useState(0);
+const [thereIsAWinner, setAWinner] = useState(false);
 
 function checkWin(){
     //console.log("reached checkwin function");
     let checkBoard = board;
     //console.log(checkBoard);
-    if (checkBoard[0] === checkBoard[1] && checkBoard[0] === checkBoard[2]){
-        console.log("someone has won the game");
+    if (checkBoard[0] != ' ' && checkBoard[0] === checkBoard[1] && checkBoard[1] === checkBoard[2]){
+        console.log("someone has won the game 1");
+        let winner = checkBoard[0];
+        console.log("the winner is: ");
+        console.log(winner);
+        //setAWinner(true);
     }
+    if (checkBoard[0] != ' ' && checkBoard[0] === checkBoard[3] && checkBoard[3] === checkBoard[6]){
+        console.log("someone has won the game 2");
+        let winner = checkBoard[0];
+        //setAWinner(true);
+
+    }
+    if (checkBoard[4] != ' ' && checkBoard[4] === checkBoard[1] && checkBoard[1] === checkBoard[7]){
+        console.log("someone has won the game 3");
+        let winner = checkBoard[4];
+        //setAWinner(true);
+
+    }
+    if (checkBoard[2] != ' ' && checkBoard[2] === checkBoard[5] && checkBoard[5] === checkBoard[8]){
+        console.log("someone has won the game 4");
+        let winner = checkBoard[2];
+        //setAWinner(true);
+
+    }
+    if (checkBoard[3] != ' ' && checkBoard[3] === checkBoard[4] && checkBoard[4] === checkBoard[5]){
+        console.log("someone has won the game 5");
+        let winner = checkBoard[3];
+        //setAWinner(true);
+
+    }
+    if (checkBoard[6] != ' ' && checkBoard[6] === checkBoard[7] && checkBoard[7] === checkBoard[8]){
+        console.log("someone has won the game 6");
+        let winner = checkBoard[6];
+        //setAWinner(true);
+
+    }
+    if (checkBoard[2] != ' ' && checkBoard[2] === checkBoard[4] && checkBoard[4] === checkBoard[6]){
+        console.log("someone has won the game 7");
+        let winner = checkBoard[2];
+        //setAWinner(true);
+     
+    } 
+    if (checkBoard[0] != ' ' && checkBoard[0] === checkBoard[4] && checkBoard[4] === checkBoard[8]){
+        console.log("someone has won the game 8");
+        let winner = checkBoard[0];
+        //setAWinner(true);
+       
+    }
+    
     
 }
 
@@ -64,6 +112,10 @@ if (plays > 4){
     checkWin();
     
 }
+
+// if (thereIsAWinner === true){
+//     console.log("reafched there is a winner");
+// }
 
 useEffect(() => {
     //listening for a new move event
